2df549c094f3b7635100739a17e08f21cda3cdbc
Shiny Alex <carvantes.alex@gmail.com>
2018-04-13 19:36:29 -0500

    Html-terminado

--- templates/servicios.php
+++ templates/servicios.php
@@ -73,7 +73,223 @@
     </div>
 </section>
 
+<section id="garantia">
+    <div class="flex">
+        <div class="col">
+            <h2>
+                <span class="light-big-white">Máquinas de Coser
+                Brother y Singer en</span>
+                <span class="bigest-white">Garantía</span>
+            </h2>        
+            <p class="white">Reparamos tus equipos de costura con refacciones originales.  En garantía o con extracoberturas AIG (garantía extendida Wall Mart, Sam´s, Aurrera, Chedarui Best Buy). </p>
+            
+            <div class="flex">
+                <div class="col">
+                    <button class="btn-sandia">¡Contáctanos!</button>
+                </div>
+                <div class="col">
+                    <button class="btn-yellow">¿Cómo llegar?</button>
+                </div>    
+            </div>
+        </div>
+        <div class="col">
+            <img src="../img/servicios/fondo-coser.jpg" alt="SINGER máquina de coser">
+        </div>
+    </div>
+</section>
 
+<section id="moviles">
+    <div class="contenedor">
+        <div class="flex">
+            <div class="col">
+                <div class="col"></div>
+                <div class="sqrt-sandia">    
+                    <h2>
+                        <span class="light-big-white">Computadoras, impresoras</span>
+                        <span class="bigest-white">Móviles </span>
+                        <span class="light-mid-white">y accesorios por cargos</span>
+                    </h2>
+                    
+                    <p class="white">Reparamos equipos de cómputo,  impresoras, multifuncionales, escaners, plotters, servidores, portátiles, tabletas,no breaks, reguladores, máquinas de escribir, rotuladores electrónicos, proyectores, pantallas y accesorios.  
+                    </p>
+                    
+                    <div class="flex">
+                        <div class="col">
+                            <button class="btn-dead-yellow">¡Contáctanos!</button>
+                        </div>
+                        <div class="col">
+                            <button class="btn-yellow">¿Cómo llegar?</button>
+                        </div>    
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section id="proyectores">
+    <div class="flex">
+        <div class="col"></div>
+        <div class="col">
+            <h2>
+                <span class="light-big-white">Televisores, bocinas y</span>
+                <span class="bigest-white shadow">proyectores</span>
+            </h2>
+            <p class="light-mid-white">
+                por cargos
+            </p>
+            <p class="white">
+                Reparamos tus equipos de TV, sistemas de entretenimiento, reproductores Bluray, bocinas,  DVD, proyectores y pantallas de las marcas Sony, Epson,  Samsung, LG, Sharp.
+
+            </p>
+            <div class="flex">
+                <div class="col">
+                    <button class="btn-dead-yellow">¡Contáctanos!</button>
+                </div>
+                <div class="col">
+                    <button class="btn-yellow">¿Cómo llegar?</button>
+                </div>    
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="que-como">
+
+        <div class="flex">
+            <div class="col">
+                <img src="../img/servicios/celular-mano-symbols.jpg" alt="celular y laptop">        
+                <h2><span class="light-mid-black"> ¿Cómo llevar mi equipo</span>
+                <span class="bigest-sandia">al servicio?</span></h2>
+                
+                <ol>
+                    <li>Respalda los datos de tu equipo.</li>
+                    <li> Libera tu equipo de contraseñas.</li>
+                    <li>Presenta tu equipo con todos sus accesorios.</li>
+                    <li> Si está en Garantía trae una copia del documento de compra.</li>
+                    <li>Si tienes una extracobertura presénta la póliza AIG Garanplus.</li>
+                </ol>       
+            </div>
+            
+            <div class="col">
+                
+                <h2><span class="light-mid-black"> ¿Qué incluye</span>
+                <span class="bigest-sandia">este servicio?</span></h2> 
+                
+                <ul>
+                    <li>Recibirás asesoría y  repuestos originales o compatibles a bajo costo.  </li>
+                    <li>Las reparaciones siempre serán realizadas por personal certificado.</li>
+                    <li>Revisarán y ajustarán los componentes de tu equipo.</li>
+                    <li>Podrás ver vía on-line el status la orden reparación.</li>
+                    <li>Recibirás respuesta a tu garantía sin costo según las políticas de cada fabricante.</li>
+                    <li>Recibirás planes de cuotas competitivas para las reparaciones de pago. </li>
+                    <li>Actualizaremos el historial del equipo con tu fabricante. </li>
+                    <li>Para tu satisfacción probaremos el equipo en tu presencia. </li>
+                </ul>
+                <img src="../img/servicios/lap-password.jpg" alt="libro y laptop">
+            </div>
+        </div>
+</section>
+
+<section id="vendemos">
+    <div class="contenedor-center">
+       <h2 class="bigest-sandia">vendemos</h2>
+        <div class="flex">
+            <div class="col gray">
+                
+                <h2 class="big-sandia">Dispositivos nuevos y remanufacturados</h2>
+                <p class="black">Ofrecemos equipos de audio y video, computadoras, teléfonos móviles, impresoras y máquinas de coser. 
+                </p>
+                <p class="light-mid-sandia">¡Todos con garantía!</p>
+                <img src="../img/servicios/iphone.png" alt="iphone">
+            </div>
+            <div class="col gray">
+                
+                <h2 class="big-sandia">Refacciones para tus Equipos</h2>
+                <p class="black">Ofrecemos a bajo costo, refaciones originales para tus equipos de las marcas Dell, Apple, Epson, Singer, Samsung, Brother, Tripp Lite, View Sonic y  HP. 
+                </p>
+                <img src="../img/servicios/motherboard.png" alt="tarjeta madre">
+            </div>
+            <div class="col gray">
+                
+                <h2 class="big-sandia">Cartuchos y tonners 100% originales</h2>
+                <p class="black">Tenemos al mejor precio, tus consumibles de impresión de todas las marcas. </p>
+                <img src="../img/servicios/cartuchos.png" alt="cartuchos de tinta">
+            </div>
+        </div>
+        
+        <div class="flex">
+            <div class="col">
+                <button class="btn-dead-yellow">¡Contáctanos!</button>
+            </div>
+            <div class="col">
+                <button class="btn-yellow">¿Cómo llegar?</button>
+            </div>    
+        </div>
+        
+    </div>
+</section>
+
+<section id="planes">
+    <div class="contenedor">
+        <div class="flex">
+            <div class="col">
+                <h2><span class="big-sandia"></span></h2>
+                <p class="black"></p>
+            </div>
+            <div class="col">
+                <h2><span class="big-sandia"></span>
+                <br>
+                <span class="bigest-sandia"></span></h2>
+                
+                <p class="big-sandia"></p>
+                <p class="black"></p>
+                
+                <div class="flex">
+                    <div class="col">
+                        <button class="btn-dead-yellow">¡Contáctanos!</button>
+                    </div>
+                    
+                    <div class="col">
+                        <button class="btn-yellow">¿Cómo llegar?</button>
+                    </div>
+                        
+                </div>
+            </div>
+        </div>
+    </div>
+</section> 
+
+<section id="soluciones-tec">
+    <div class="contenedor-center">
+        <h2><span class="big-white">Soluciones en</span><br><span class="bigest-white">Tecnología</span></h2>
+        <div class="flex">
+            <div class="col">
+                <img src="../img/servicios/tuberia.jpg" alt="cableado certificado">
+                <h3 class="big-white">Diseño e instalación de cableado estructurado </h3>
+                <p class="white">Ofrecemos cableado estructurado para voz y datos.  
+                Certificado por las marcas Condunet (Condumex), Belden y HUbbel. </p>
+                <button class="btn-dead-yellow">¡Contáctanos!</button>
+            </div>
+            <div class="col">
+                <img src="../img/servicios/video-wall.jpg" alt="video wall">
+                <h3 class="big-white">Diseño e implemantación de Video Wall</h3>
+                <p class="white">Representamos marcas como Samsung, LG y Viewsonic
+                 ¡Consigue tu video wall hoy!</p>
+                <button class="btn-dead-yellow">¡Contáctanos!</button>
+            </div>
+            <div class="col">
+                <img src="../img/servicios/digital-signals.jpg" alt="señalización digital">
+                <h3 class="big-white">Diseño e instalación de Señalización digital</h3>
+                <p class="white">os un proyecto personalizado de transisión dinámica audiovisual, para tus pantallas. 
+                 </p>
+                <button class="btn-dead-yellow">¡Contáctanos!</button>
+            </div>
+        </div>
+    </div>
+</section>   
 
 <?php 
     get_footer();
